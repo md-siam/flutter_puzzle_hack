0232e33f88d202192de0b00355b9ed4e14fecaba
Md. Siam <md.siam03@gmail.com>
2022-03-10 15:29:33 +0600

    top_appbar fixed

--- lib/app/view/puzzle_game.dart
+++ lib/app/view/puzzle_game.dart
@@ -1,18 +1,37 @@
+import 'package:clay_containers/widgets/clay_container.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_puzzle_hack/app/widget/top_appbar.dart';
 
-class PuzzleGame extends StatelessWidget {
+import '/app/widget/top_appbar.dart';
+
+class PuzzleGame extends StatefulWidget {
   const PuzzleGame({Key? key}) : super(key: key);
 
   @override
+  State<PuzzleGame> createState() => _PuzzleGameState();
+}
+
+class _PuzzleGameState extends State<PuzzleGame> {
+  @override
   Widget build(BuildContext context) {
+    var size = MediaQuery.of(context).size;
     return Scaffold(
+      appBar: const TopAppBar(),
       body: SafeArea(
+        child: Center(
           child: Column(
-        children: [
-          TopButtons(),
-        ],
-      )),
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              ClayContainer(
+                emboss: true,
+                color: Theme.of(context).primaryColor,
+                width: size.height * 0.5,
+                height: size.height * 0.5,
+                borderRadius: 10,
+              ),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
--- lib/app/widget/top_appbar.dart
+++ lib/app/widget/top_appbar.dart
@@ -6,35 +6,34 @@ import 'dropdown_menu.dart';
 import '../provider/theme_provider.dart';
 import '../provider/sound_provider.dart';
 
-class TopButtons extends StatelessWidget {
-  const TopButtons({
+class TopAppBar extends StatelessWidget with PreferredSizeWidget {
+  const TopAppBar({
     Key? key,
   }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Row(
-      children: [
-        const SizedBox(width: 10),
-        Consumer<ThemeProvider>(
-          builder: (context, themeProvider, child) {
-            return Consumer<SoundProvider>(
-              builder: (context, soundProvider, child) {
-                return DayNightSwitcherIcon(
-                  dayBackgroundColor: const Color(0xFF0C91D6),
-                  isDarkModeEnabled: themeProvider.darkTheme,
-                  onStateChanged: (value) {
-                    themeProvider.darkTheme
-                        ? soundProvider.playLightSound()
-                        : soundProvider.playDarkSound();
-                    themeProvider.toggleTheme();
-                  },
-                );
-              },
-            );
-          },
-        ),
-        const Expanded(child: SizedBox()),
+    return AppBar(
+      leading: Consumer<ThemeProvider>(
+        builder: (context, themeProvider, child) {
+          return Consumer<SoundProvider>(
+            builder: (context, soundProvider, child) {
+              return DayNightSwitcherIcon(
+                dayBackgroundColor: const Color(0xFF0C91D6),
+                isDarkModeEnabled: themeProvider.darkTheme,
+                onStateChanged: (value) {
+                  themeProvider.darkTheme
+                      ? soundProvider.playLightSound()
+                      : soundProvider.playDarkSound();
+                  themeProvider.toggleTheme();
+                },
+              );
+            },
+          );
+        },
+      ),
+      title: const Text('Puzzle Hack'),
+      actions: [
         CustomDropdownMenu(
           borderRadius: BorderRadius.circular(10),
           backgroundColor: Theme.of(context).hoverColor,
@@ -44,8 +43,10 @@ class TopButtons extends StatelessWidget {
           ],
           onChange: (index) {},
         ),
-        const SizedBox(width: 2.0),
       ],
     );
   }
+
+  @override
+  Size get preferredSize => const Size.fromHeight(kToolbarHeight);
 }
